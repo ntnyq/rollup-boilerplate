3ea132f8f5e06953a61eec936e6f391d9033b259
ntnyq <ntnyq13@gmail.com>
2023-07-03 15:01:14 +0800

    chore: bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      reporter: [`lcov`, `text`, `html`, `json`, `clover`],
+      provider: 'v8',
+      reporter: ['clover', 'json', 'text', 'lcov', 'html'],
     },
   },
 })
